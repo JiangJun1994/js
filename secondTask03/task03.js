0c7ed416a14d73afdcf5d6a9b29d454a1d460ca6
JiangJun1994 <925736803@qq.com>
2017-03-19 15:31:51 +0800

    done

--- secondTask03/task03.js
+++ secondTask03/task03.js
@@ -23,7 +23,7 @@ window.onload=function(){
   function showOptions(){
     var schoolList=[["北京邮电大学","北京大学","清华大学","北京航空航天大学","北京交通大学","北京理工大学","北京科技大学"],
       ["南开大学","天津大学","天津科技大学","天津工业大学","中国民航大学","天津理工大学","天津农学院"],
-      ["复旦大学","同济大学","上海交通大学","华东理工大学","上海理工大学","上海海事大学","东华大学"]]
+      ["复旦大学","同济大学","上海交通大学","华东理工大学","上海理工大学","上海海事大学","东华大学","上海大学"]]
     var index=city.selectedIndex-1;
     var schoolname=schoolList[index];
     for(var i=0;i<schoolname.length;i++){
