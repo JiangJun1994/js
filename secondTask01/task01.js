ca8dd582e64476ab584e39290ef8d33e19dbc660
JiangJun1994 <925736803@qq.com>
2017-03-19 15:14:08 +0800

    done

--- secondTask01/task01.js
+++ secondTask01/task01.js
@@ -6,6 +6,7 @@ window.onload=function () {
   var btnNickName=document.getElementById("btnNickName");
   function check1(){
     var inputNick=document.getElementById("nickName");
+    var tip=document.getElementById("tip");
     var form1=document.getElementsByClassName("form")[0];
     var len=inputNick.value.length;
     var text=0;
@@ -17,24 +18,39 @@ window.onload=function () {
         text=text+2;
       }
     };
-    if(text<4||text>16){
-      var p=document.createElement("p");
-      var alarm=document.createTextNode("必填，长度为4~16个字符");
-      p.appendChild(alarm);
-      if(form1.children[3]){
-        form1.removeChild(form1.children[3])
-      }
-      form1.appendChild(p);
+    if(text==0){
+      tip.innerHTML="名称不能为空！"
+      tip.style.color="red";
+      inputNick.style.border="1px solid red"
+      // var p=document.createElement("p");
+      // var alarm=document.createTextNode("名称不能为空！");
+      // p.appendChild(alarm);
+      // if(form1.children[3]){
+      //   form1.removeChild(form1.children[3])
+      // }
+      // form1.appendChild(p);
+    }else if(text<4||text>16){
+      tip.innerHTML="格式错误"
+      tip.style.color="red";
+      inputNick.style.border="1px solid red"
+      // var p=document.createElement("p");
+      // var alarm=document.createTextNode("格式错误");
+      // p.appendChild(alarm);
+      // if(form1.children[3]){
+      //   form1.removeChild(form1.children[3])
+      // }
+      // form1.appendChild(p);
     }else{
-      var p2=document.createElement("p");
-      var alarm2=document.createTextNode("名称格式正确");
-      p2.appendChild(alarm2);
-      if(form1.children[3]){
-        form1.removeChild(form1.children[3])
-      }
-      form1.appendChild(p2);
+      tip.innerHTML="名称格式正确"
+      // var p2=document.createElement("p");
+      // var alarm2=document.createTextNode("名称格式正确");
+      // p2.appendChild(alarm2);
+      // if(form1.children[3]){
+      //   form1.removeChild(form1.children[3])
+      // }
+      // form1.appendChild(p2);
       inputNick.style.border="1px solid green";
-      p2.style.color="green";
+      tip.style.color="green";
     }
   }
   btnNickName.onclick=check1;
